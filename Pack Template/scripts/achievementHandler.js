4b7121f4b666e7180f718ebdc2192980cb42a851
camerin <camerin.hahn@gmail.com>
2024-02-14 15:42:57 -0800

    Added Get all achievments

--- Pack Template/scripts/achievementHandler.js
+++ Pack Template/scripts/achievementHandler.js
@@ -1,6 +1,12 @@
 export class achievementHandler{
 	constructor(achievmentObject){
 		this.achievments=achievmentObject;
+		this.dynamicProperties=[]
+		for (const [key, value] of Object.entries(this.achievments)) {
+			if(!this.dynamicProperties.includes(value.container){
+				this.dynamicProperties.push(value.container)
+			}
+		}
 	}
 	checkAchievment(handle,player){
 		const achievmentDefs = this.achievments[handle];
@@ -38,6 +44,21 @@ export class achievementHandler{
 	getDisplayString(handle,player){
 		const status = this.checkAchievment(handle,player)
 		
+	}
+	getAllAchievmentData(handle,player){
+		let playerData={}
+		for (let dynamicProperty of this.dynamicProperties){
+			playerData[dynamicProperty]=player.getDynamicProperty(dynamicProperty)
+		}
+		let achievmentData={}
+		for (const [achievmentHandle, achievementDef] of Object.entries(this.achievments)) {
+			if (!(achievementDef.category in achievmentData)){
+				achievmentData[category]={}
+			}
+			achievmentData[category][achievmentHandle]=achievementDef
+			achievmentData[category][achievmentHandle].unlocked=playerData[achievementDef.container]&0b1<<achievementDef.containerIndex > 0
+		}
+		return achievmentData
 	}
 	achievmentUnlock(player,str){
 		let display=player.onScreenDisplay
